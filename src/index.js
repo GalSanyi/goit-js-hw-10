a463164e4b2504d6eff7563b14cc282c63b2c16a
GalSanyi <galsanyi84@gmail.com>
2021-12-16 18:35:29 +0100

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import countryCard from '../tamplates/city-card.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const refs = {
+
+
+    input: document.querySelector('#search-box'),
+    countryList: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('.country-info'),
+}
+
+
+
+
+fetch('https://restcountries.com/v3.1/all')
+    .then(response => {
+        return response.json();
+    })
+    .then(country => {
+        // console.log(country);
+        const markup = countryCard(country);
+        console.log(markup);
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
